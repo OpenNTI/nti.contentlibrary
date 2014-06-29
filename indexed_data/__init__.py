662df01122e5e86c4847b74a517e92260a3fa27a
Jason Madden <jason.madden@nextthought.com>
2014-06-29 20:07:53 +0000

    Kill the global XXXIndexMap utilities in favor of local annotations that play well with on-the-fly updating, a hierarchy of libraries, and persistence. I'm still not really happy with the concepts since you have to know about them and there's some overlap with 'embeddedContainerNTIIDs', but the implementation at least is no longer redundant. Also, potentially fixes a bug getting dashboard info for videos
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/NextThoughtPlatform/trunk/nti.dataserver/src/nti/contentlibrary@42291 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ indexed_data/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Indexed data is associated with content units and is typically
+extracted at content rendering time.
+
+It is typically accessed by adapting content units to the interfaces
+of this package.
+
+
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
