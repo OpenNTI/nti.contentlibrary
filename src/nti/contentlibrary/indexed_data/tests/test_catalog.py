15b1effb5b0374fe6f932e69c83569c818cddb9b
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-01-26 23:09:44 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary/trunk@105273 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/indexed_data/tests/test_catalog.py
+++ src/nti/contentlibrary/indexed_data/tests/test_catalog.py
@@ -11,33 +11,37 @@ from hamcrest import is_
 from hamcrest import has_length
 from hamcrest import assert_that
 
-from nti.contentlibrary.indexed_data.catalog import create_library_catalog
+from nti.contentlibrary.indexed_data.catalog import create_assets_library_catalog
 
 from nti.contentlibrary.tests import ContentlibraryLayerTest
 
+
 class TestCatalog(ContentlibraryLayerTest):
-	
-	def test_catalog(self):
-		catalog = create_library_catalog()
 
-		catalog.container_index.do_index_doc(1, ('a','b'))
-		assert_that(list(catalog.get_references(container_ntiids='a')), is_([1]))
-		
-		catalog.unindex_doc(1)
-		assert_that(list(catalog.get_references(container_ntiids='b')), is_([]))
-		assert_that(list(catalog.get_references(container_ntiids='a')), is_([]))
+    def test_catalog(self):
+        catalog = create_assets_library_catalog()
 
-		catalog.container_index.do_index_doc(1, ('x','y'))
-		assert_that(catalog.get_containers(1), has_length(2))
-		
-		catalog.update_containers(1, ('z',))
-		assert_that(catalog.get_containers(1), has_length(3))
-		
-		catalog.update_containers(1, ('z',))
-		assert_that(catalog.get_containers(1), has_length(3))
-		
-		catalog.remove_containers(1, ('x','y'))
-		assert_that(catalog.get_containers(1), has_length(1))
-		
-		catalog.remove_all_containers(1)
-		assert_that(catalog.get_containers(1), has_length(0))
+        catalog.container_index.do_index_doc(1, ('a', 'b'))
+        assert_that(list(catalog.get_references(container_ntiids='a')), 
+					is_([1]))
+
+        catalog.unindex_doc(1)
+        assert_that(list(catalog.get_references(container_ntiids='b')), 
+					is_([]))
+        assert_that(list(catalog.get_references(container_ntiids='a')),
+					is_([]))
+
+        catalog.container_index.do_index_doc(1, ('x', 'y'))
+        assert_that(catalog.get_containers(1), has_length(2))
+
+        catalog.update_containers(1, ('z',))
+        assert_that(catalog.get_containers(1), has_length(3))
+
+        catalog.update_containers(1, ('z',))
+        assert_that(catalog.get_containers(1), has_length(3))
+
+        catalog.remove_containers(1, ('x', 'y'))
+        assert_that(catalog.get_containers(1), has_length(1))
+
+        catalog.remove_all_containers(1)
+        assert_that(catalog.get_containers(1), has_length(0))
