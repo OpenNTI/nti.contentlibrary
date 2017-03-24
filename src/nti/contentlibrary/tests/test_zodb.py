accab93135b168284a4dcc2add95efbb97863306
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-24 01:45:16 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary/trunk@109514 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/tests/test_zodb.py
+++ src/nti/contentlibrary/tests/test_zodb.py
@@ -23,9 +23,9 @@ from nti.externalization.externalization import to_external_object
 from nti.externalization.internalization import find_factory_for
 from nti.externalization.internalization import update_from_external_object
 
-from nti.contentlibrary.tests import ContentlibraryLayerTest
-
 from nti.recorder.adapters import ITransactionRecordHistory
+
+from nti.contentlibrary.tests import ContentlibraryLayerTest
 
 
 class TestZODB(ContentlibraryLayerTest):
