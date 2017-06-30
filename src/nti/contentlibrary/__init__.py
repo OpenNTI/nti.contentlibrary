e5c953b8bef3bec3df49c85fbf4ca91bd5938029
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-30 21:09:03 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary/trunk@116387 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/__init__.py
+++ src/nti/contentlibrary/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Support for defining and traversing libraries composed of
-independent (but possibly related and/or linked) units of content.
-
 .. $Id$
 """
 
