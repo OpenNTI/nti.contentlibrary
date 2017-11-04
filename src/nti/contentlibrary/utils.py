0c32c479e19efb245db665eabb220158eedb7ae3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-11-04 11:19:22 +0000

    pass proper zip
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.contentlibrary/trunk@123726 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/contentlibrary/utils.py
+++ src/nti/contentlibrary/utils.py
@@ -289,11 +289,7 @@ def is_valid_presentation_assets_source(source, versions=None, tmpdirs=None):
                     if files and len(files) == 1:
                         source = os.path.join(tmpdir, files[0])
                     else:
-                        for name in files or ():
-                            if name.startswith('__'):
-                                continue
-                            source = os.path.join(tmpdir, name)
-                            break  # first found
+                        source = tmpdir
             if not os.path.isdir(source):
                 tmpdirs.append(tmpdir) if tmpdir else ()
             else:  # directory
